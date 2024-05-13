c979a1c422146006019c70f2788e1cf20e2f4cc1
Linus Wæhler <linus.wahler@gmail.com>
2024-05-13 22:09:53 +0200

    This seems good

--- model.go
+++ model.go
@@ -7,22 +7,23 @@ import (
 )
 
 type MainModel struct {
-	chatModels []ChatModel
+	chatModels map[string]ChatModel
+	activeChat string
 	textInput  textinput.Model
-	activeChat int
 	isTyping   bool
 }
 
 func NewMainModel() MainModel {
 	ti := textinput.New()
 	ti.Placeholder = "forsen"
-	ti.Focus()
 	ti.CharLimit = 25 // Twitch username limit
 	ti.Width = 20
 
 	return MainModel{
-		textInput: ti,
-		isTyping:  false,
+		chatModels: make(map[string]ChatModel),
+		activeChat: "",
+		textInput:  ti,
+		isTyping:   false,
 	}
 }
 
@@ -30,48 +31,56 @@ func (m MainModel) Init() tea.Cmd {
 	return textinput.Blink // Cursor blink start
 }
 
+func (m MainModel) channelExists(channel string) bool {
+	_, exists := m.chatModels[channel]
+	return exists
+}
+
 func (m MainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmds []tea.Cmd
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "q", "esc", "ctrl+c":
-			return m, tea.Quit
-		case "a":
-			// TextInput pop up
-			m.isTyping = true
-			return m, nil
-		case "enter":
-			if m.isTyping {
+		if m.isTyping {
+			// Handle text input updates
+			var cmd tea.Cmd
+			m.textInput, cmd = m.textInput.Update(msg)
+			cmds = append(cmds, cmd)
+			switch msg.String() {
+			case "esc":
 				m.isTyping = false
+			case "enter":
+				m.isTyping = false
+				username := m.textInput.Value()
 
-				// Create a new ChatModel with the username
-				chatModel := NewChatModel(twitch.NewAnonymousClient(), NewStyledSpinner(), m.textInput.Value())
-
-				m.chatModels = append(m.chatModels, chatModel)
-
-				// Set that chat as the active chat
-				// This is a bit of a hack, but it works for now
-				m.activeChat = len(m.chatModels) - 1
-
-				return m, m.chatModels[m.activeChat].Init()
+				// If channel already exists, just swap to that model
+				if m.channelExists(username) {
+					m.activeChat = username
+				} else {
+					// Create a new ChatModel with the username
+					chatModel := NewChatModel(twitch.NewAnonymousClient(), NewStyledSpinner(), username)
+					m.chatModels[username] = chatModel
+					m.activeChat = username
+					cmds = append(cmds, chatModel.Init())
+				}
+			}
+		} else {
+			// Handle non-input key presses
+			switch msg.String() {
+			case "q", "ctrl+c":
+				cmds = append(cmds, tea.Quit)
+			case "a":
+				// TextInput pop up
+				m.isTyping = true
+				cmds = append(cmds, m.textInput.Focus())
 			}
 		}
-	}
-
-	// Propogate update to activeChat
-	if len(m.chatModels) > 0 {
-		updatedModel, cmd := m.chatModels[m.activeChat].Update(msg)
-		m.chatModels[m.activeChat] = updatedModel.(ChatModel)
-		cmds = append(cmds, cmd)
-	}
-
-	// Might be a silly way to do this, but we're doing our best
-	if m.isTyping {
-		var cmd tea.Cmd
-		m.textInput, cmd = m.textInput.Update(msg)
-		return m, cmd
+	default:
+		if m.channelExists(m.activeChat) {
+			updatedModel, cmd := m.chatModels[m.activeChat].Update(msg)
+			m.chatModels[m.activeChat] = updatedModel.(ChatModel)
+			cmds = append(cmds, cmd)
+		}
 	}
 
 	return m, tea.Batch(cmds...)
